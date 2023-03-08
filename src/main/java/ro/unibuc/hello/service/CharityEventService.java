1ff68c9f302c75b780795d8c5e325cc573722d40
Andrei Gatej <andreigtj01@gmail.com>
2023-03-08 16:24:22 +0200

    feat(charity): create one

---
+++ src/main/java/ro/unibuc/hello/service/CharityEventService.java
@@ -0,0 +1,25 @@
+package ro.unibuc.hello.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import ro.unibuc.hello.data.CharityEventEntity;
+import ro.unibuc.hello.data.CharityEventRepository;
+import ro.unibuc.hello.dto.CreateCharityDTO;
+
+@Component
+public class CharityEventService {
+
+  @Autowired
+  private CharityEventRepository charityEventRepository;
+
+  public void createCharityEvent (CreateCharityDTO charityData) {
+    var newCharity = new CharityEventEntity(
+      charityData.name,
+      charityData.location,
+      charityData.date
+    );
+    
+    charityEventRepository.insert(newCharity);
+  }
+}
