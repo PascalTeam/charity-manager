a24c451b7492ea87b91841ae452fd7e7024b8091
Luca Trușcă <t.luca1152@gmail.com>
2023-03-12 21:10:21 +0200

    feat(product): create ProductService

--- src/main/java/ro/unibuc/hello/service/ProductService.java
+++ src/main/java/ro/unibuc/hello/service/ProductService.java
@@ -1,13 +1,39 @@
 package ro.unibuc.hello.service;
 
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ro.unibuc.hello.data.CharityEventRepository;
+import ro.unibuc.hello.data.ProductEntity;
 import ro.unibuc.hello.dto.ProductDTO;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
+  @Autowired
+  private CharityEventRepository charityEventRepository;
+
   public void addProductsToCharity(String charityId, List<ProductDTO> products) {
-    // TODO
+    var charity = charityEventRepository.findById(charityId);
+    if (charity.isEmpty()) {
+      return;
+    }
+
+    var productEntities = products.stream()
+        .map(productDTO -> {
+          var productEntity = new ProductEntity(productDTO.name, productDTO.quantity);
+
+          var id = new ObjectId();
+          productEntity.setId(id.toString());
+
+          return productEntity;
+        })
+        .collect(Collectors.toList());
+
+    charity.get().products.addAll(productEntities);
+
+    charityEventRepository.save(charity.get());
   }
 }
