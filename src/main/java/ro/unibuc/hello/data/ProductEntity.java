4de9d3ecb28bb80c4a3a4c56a7fb71c5869bfaf3
Luca Trușcă <t.luca1152@gmail.com>
2023-03-12 20:57:21 +0200

    feat(product): create ProductEntity

--- src/main/java/ro/unibuc/hello/data/ProductEntity.java
+++ src/main/java/ro/unibuc/hello/data/ProductEntity.java
@@ -1,5 +1,31 @@
 package ro.unibuc.hello.data;
 
+import org.springframework.data.annotation.Id;
+
 public class ProductEntity {
-  // TODO
+  @Id
+  public String id;
+
+  public String name;
+  public int quantity;
+
+  public ProductEntity() {
+  }
+
+  public ProductEntity(String name, int quantity) {
+    this.name = name;
+    this.quantity = quantity;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  @Override
+  public String toString() {
+    return String.format(
+        "Product[id=%s, name='%s', quantity='%s']",
+        id, name, quantity
+    );
+  }
 }
