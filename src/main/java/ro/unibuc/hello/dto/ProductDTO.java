33e3ee1d5da78c730d296c4ecd5c14c3f6b16e23
Luca Trușcă <t.luca1152@gmail.com>
2023-03-12 20:59:27 +0200

    feat(product): create ProductDTO

--- src/main/java/ro/unibuc/hello/dto/ProductDTO.java
+++ src/main/java/ro/unibuc/hello/dto/ProductDTO.java
@@ -1,5 +1,23 @@
 package ro.unibuc.hello.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ProductDTO {
-  // TODO
+  public String name;
+  public int quantity;
+
+  @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+  public ProductDTO(@JsonProperty("name") String name, @JsonProperty("quantity") int quantity) {
+    this.name = name;
+    this.quantity = quantity;
+  }
+
+  @Override
+  public String toString() {
+    return "ProductDTO[" +
+        "name='" + name + '\'' +
+        ", quantity=" + quantity +
+        ']';
+  }
 }
